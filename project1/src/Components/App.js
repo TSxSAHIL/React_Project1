a843ef683a7df14c654a837ed50316758a887275
TSxSAHIL <tssahil2001@gmail.com>
2024-03-21 12:39:06 +0530

    learned upto useEffect()

--- project1/src/Components/App.js
+++ project1/src/Components/App.js
@@ -12,6 +12,7 @@ function App() {
     console.log(contact);
     setContacts([...contacts , contact]);
   } 
+  
 
   useEffect(() =>{
     localStorage.setItem(LOCAL_STORAGE_KEY , JSON.stringify(contacts));
